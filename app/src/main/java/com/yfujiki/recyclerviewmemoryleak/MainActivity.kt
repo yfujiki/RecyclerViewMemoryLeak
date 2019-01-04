5477f19658b1fca6de241ea92d85c01389e8da91
Yuichi Fujiki <yuichi@chaione.com>
2019-01-04 09:47:17 -0700

    Implement finalize to check memory leak.

--- app/src/main/java/com/yfujiki/recyclerviewmemoryleak/MainActivity.kt
+++ app/src/main/java/com/yfujiki/recyclerviewmemoryleak/MainActivity.kt
@@ -9,4 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    protected fun finalize() {
+        println("Activity was reclaimed")
+    }
 }
